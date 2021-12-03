726fbad7af921c37fd3e01c651e4fac511c22fa6
samankassou <samankassoufoulla@gmail.com>
2021-12-03 14:14:13 +0100

    Login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,13 +2,38 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function index()
     {
         return view('auths.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+
+            return redirect()->route('login')->withInput()->withErrors([
+                'email' => __('auth.failed')
+            ]);
+        }
+
+        $route = 'home';
+        if(auth()->user()->role == 'admin'){
+            $route = 'admin.dashboard';
+        }else if(auth()->user()->role == 'moderator'){
+            $route = 'moderator.dashboard';
+        }
+
+        return redirect()->intended(route($route));
     }
 }
--- routes/auth.php
+++ routes/auth.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('login');
