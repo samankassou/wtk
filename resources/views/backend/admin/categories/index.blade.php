7b74a0995683c37e0fdbe5b498ac507db72126b2
samankassou <samankassoufoulla@gmail.com>
2021-12-05 16:33:25 +0100

    wip

--- resources/views/backend/admin/categories/index.blade.php
+++ resources/views/backend/admin/categories/index.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
-@section('title', 'Adverts')
+@section('title', 'Categories')
 
 @section('content')
 <section class="section">
     <div class="section-header">
-        <h1>Adverts</h1>
+        <h1>Categories</h1>
     </div>
 
     <div class="section-body">
-        <h2 class="section-title">List of Adverts</h2>
-        <p class="section-lead">This page is for adverts registered in the system.</p>
+        <h2 class="section-title">List of Categories</h2>
+        <p class="section-lead">This page is for categories registered in the system.</p>
         <div class="card">
             <div class="card-body p-0">
                 <div class="table-responsive">
@@ -19,21 +19,19 @@
                             <tr>
                                 <th>#</th>
                                 <th>Name</th>
-                                <th>Number of neighborhoods</th>
                                 <th></th>
                             </tr>
                         </thead>
                         <tbody>
-                            @forelse ($adverts as $advert)
+                            @forelse ($categories as $category)
                             <tr>
                                 <td>1</td>
                                 <td>Lorem</td>
                                 <td></td>
-                                <td></td>
                             </tr>
                             @empty
                                 <tr>
-                                    <td colspan="4" class="text-center">No data found</td>
+                                    <td colspan="3" class="text-center">No data found</td>
                                 </tr>
                             @endforelse
                         </tbody>
--- routes/admin.php
+++ routes/admin.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('dashboard', [AdminController::class, 'index'])->name('dashboard');
+Route::get('adverts', [AdminController::class, 'adverts'])->name('adverts');
+Route::get('categories', [AdminController::class, 'categories'])->name('categories');
 Route::get('cities', [AdminController::class, 'cities'])->name('cities');
 Route::get('users', [AdminController::class, 'users'])->name('users');
