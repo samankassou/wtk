9a006b25d78bd32c46ce80a13162e185c33b320e
samankassou <samankassoufoulla@gmail.com>
2021-12-23 08:03:59 +0100

    Make temporary files prunable

--- app/Models/TmpFile.php
+++ app/Models/TmpFile.php
@@ -5,23 +5,38 @@ namespace App\Models;
 use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Support\Facades\File;
 
 class TmpFile extends Model
 {
-    use HasFactory;
+    use HasFactory, Prunable;
 
     protected $fillable = ['folder', 'name'];
 
-    protected static function boot(){
+    protected static function boot()
+    {
         parent::boot();
-        static::deleting(function($file){
-            try{
-                File::deleteDirectory(storage_path('app/public/uploads/tmp/'.$file->folder));
-            }catch(Exception $e){
+        static::deleting(function ($file) {
+            try {
+                File::deleteDirectory(storage_path('app/public/uploads/tmp/' . $file->folder));
+            } catch (Exception $e) {
                 throw new Exception($e->getMessage(), 1);
-
             }
         });
+    }
+
+    public function prunable()
+    {
+        return $this->where('created_at', '<=', now()->subDays());
+    }
+
+    protected function pruning()
+    {
+        try {
+            File::deleteDirectory(storage_path('app/public/uploads/tmp/' . $this->folder));
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage(), 1);
+        }
     }
 }
